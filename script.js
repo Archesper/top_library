65862d6f1c39f46e73f528417784f36d9fb294e6
Arch <wbhaoui@gmail.com>
2023-03-21 23:56:25 +0000

    Add displayBooks function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let myLibrary = [];
 
+// temporary to try out displayBooks()
+myLibrary.push(new Book("The Book of Life", "Little Mix", 77, true));
+myLibrary.push(new Book("The Book of Life", "Little Mix", 77, true));
+myLibrary.push(new Book("The Book of Life", "Little Mix", 77, false));
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -8,6 +13,35 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read))
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
+function displayBooks(books) {
+  const grid = document.querySelector(".card_grid");
+  books.forEach((book) => {
+    // Create html elements
+    const card = document.createElement("div");
+    const title = document.createElement("h1");
+    const author = document.createElement("h2");
+    const pages = document.createElement("p");
+    const read_status = document.createElement("button");
+    const remove = document.createElement("button");
+    // Add classes
+    card.classList.add("card");
+    title.classList.add("title");
+    author.classList.add("author");
+    pages.classList.add("pages");
+    read_status.classList.add(book.read ? "read" : "unread");
+    // Add content
+    title.textContent = '"' + book.title + '"';
+    author.textContent = book.author;
+    pages.textContent = book.pages + " pages";
+    read_status.textContent = book.read ? "Read" : "Not Read"
+    remove.textContent = "Remove"
+    card.append(title, author, pages, read_status, remove);
+    // Add card to grid
+    grid.append(card);
+  });
+}
+
+displayBooks(myLibrary)
