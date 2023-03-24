0bf7bdfd8e2ee5947d5221f5f9b1ee46f85fcf5c
Arch <wbhaoui@gmail.com>
2023-03-24 13:45:37 +0000

    Add add book, toggle read status and remove book features

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ const modal = document.querySelector("dialog");
 add_book.addEventListener("click", () => {
   modal.showModal();
 });
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
+});
+
+// Add form event listener
+const form = document.querySelector("dialog form");
+form.addEventListener("submit", displayNewBook);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,13 +28,18 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
 function displayBooks(books) {
   const grid = document.querySelector(".card_grid");
-  books.forEach((book) => {
+  const current_display = grid.childElementCount;
+  books.slice(current_display).forEach((book, index) => {
     // Create html elements
     const card = document.createElement("div");
     const title = document.createElement("h1");
@@ -39,6 +53,9 @@ function displayBooks(books) {
     author.classList.add("author");
     pages.classList.add("pages");
     read_status.classList.add(book.read ? "read" : "unread");
+    // Add event listeners
+    remove.addEventListener("click", removeBook);
+    read_status.addEventListener("click", toggleReadStatus)
     // Add content
     title.textContent = '"' + book.title + '"';
     author.textContent = book.author;
@@ -46,9 +63,44 @@ function displayBooks(books) {
     read_status.textContent = book.read ? "Read" : "Not Read";
     remove.textContent = "Remove";
     card.append(title, author, pages, read_status, remove);
+    card.dataset.index = index + current_display;
     // Add card to grid
     grid.append(card);
   });
+}
+
+function displayNewBook(e) {
+  e.preventDefault();
+  modal.close();
+  const data = form.elements;
+  addBookToLibrary(
+    data["title"].value,
+    data["author"].value,
+    data["pages"].value,
+    data["read"].checked
+  );
+  displayBooks(myLibrary);
+  form.reset();
+}
+
+function removeBook(e) {
+  const bookToRemove = e.target.parentNode;
+  bookToRemove.remove();
+  myLibrary.splice(bookToRemove.dataset.index, 1);
+}
+
+function toggleReadStatus(e) {
+  const bookDOMNode = e.target.parentNode;
+  console.log(bookDOMNode);
+  const bookObject = myLibrary[bookDOMNode.dataset.index];
+  const readStatusButton =
+    bookDOMNode.querySelector(".unread") || bookDOMNode.querySelector(".read");
+    readStatusButton.classList.toggle("read");
+  readStatusButton.classList.toggle("unread");
+  readStatusButton.textContent = readStatusButton.classList.contains("read")
+    ? "Read"
+    : "Not Read";
+  bookObject.toggleRead();
 }
 
 displayBooks(myLibrary);
